c7dcb3fca78173d1d44dfb67933625e25679f06d
Abibibi <abeba.ngwe@gmail.com>
2020-02-14 00:41:00 +0100

    data ok

---
+++ src/data/projects.js
@@ -0,0 +1,68 @@
+export default [
+  {
+    id: 1,
+    name: 'Messagerie d\'entreprise orientée teambuilding',
+    description: 'Gratitude est une application où les collaborateurs d\'une même équipe peuvent s\'envoyer des messages d\'encouragement ou de reconnaissance de façon anonyme. Rien de calculé, que de l\'honnêteté.',
+    technology: [
+      'Express.js',
+      'MySQL',
+      'Vue.js',
+    ],
+    demo: 'https://www.youtube.com/embed/lCcBEDPTk4o',
+    github: 'https://github.com/Abibibi/teambuilding-messaging-tool',
+  },
+  {
+    id: 2,
+    name: 'Réseau social autour des séries',
+    description: 'Serial Killer est à la fois une bibliothèque d\'information et un réseau social sur les séries, où les utilisateurs peuvent rédiger des, chatter et s\'abonner les uns aux autres.',
+    technology: [
+      'Express.js',
+      'PostgreSQL',
+      'Sequelize',
+      'React',
+    ],
+    site: 'http://serialkiller.abebangwe.com/',
+    demo: 'https://www.youtube.com/watch?v=36cu4wNc2OA',
+    github: 'https://github.com/Abibibi/tv-show-social-network',
+  },
+  {
+    id: 3,
+    name: 'Compte à rebours de voyage',
+    description: 'Après avoir renseigné le lieu et la date de ses prochaines vacances, l\'utilisateur-rice accède à un compte à rebours d\'ici la date de son séjour et à une belle photo de sa destination. Idéal en tant que page d\'accueil.',
+    technology: [
+      'Express.js',
+      'PostgreSQL',
+      'Vue.js',
+    ],
+    demo: 'https://www.linkedin.com/posts/abebangwe_javascript-vuejs-nodejs-activity-6627935589566038016-guO2',
+    github: 'https://github.com/Abibibi/next-trip-countdown',
+  },
+  {
+    id: 4,
+    name: 'Jeu de prédictions pour 2020',
+    technology: [
+      'JavaScript Vanilla',
+    ],
+    description: 'Au clic sur le bouton rejouer, une nouvelle prédiction pour 2020 apparaît. Inspiré du filtre Instagram "In 2020 I will be".',
+    demo: 'https://www.linkedin.com/posts/abebangwe_javascript-ugcPost-6631169661461377024-jmVz',
+    github: 'https://github.com/Abibibi/2020-prediction-game',
+  },
+  {
+    id: 5,
+    name: 'Assistant d\'impression',
+    technology: [
+      'JavaScript Vanilla',
+    ],
+    description: 'Cet outil génère une suite de chiffres pairs ou impairs. Pratique pour imprimer de gros volumes de documents en recto-verso, notamment des Google documents.',
+    github: 'https://github.com/Abibibi/printing-assistant',
+  },
+  {
+    id: 6,
+    name: 'Moteur de recherche de produits',
+    technology: [
+      'JavaScript Vanilla',
+    ],
+    description: 'Après avoir sélectionné une catégorie de produit et renseigné un budget maximal, une liste de produits correspondant aux critères saisis apparaît.',
+    github: 'https://github.com/Abibibi/product-finder',
+  },
+];

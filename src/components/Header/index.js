af9e09ef55de2df9f48f73a1cc08cb568b2180b8
Abibibi <abeba.ngwe@gmail.com>
2020-02-13 23:40:05 +0100

    header text and picture ok

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,12 +1,16 @@
 import React from 'react';
 
 import './header.scss';
+import picture from 'src/styles/assets/images/abeba_ngwe.png';
 
 const Header = () => (
   <div className="header">
     <div className="header-text">
       <p className="header-text-name">Abeba Ngwe</p>
-      <p className="header-text-subtitle">Je suis développeuse Node.js / React / Vue.js. Pour voir mon portfolio, suivez mon regard.</p>
+      <p className="header-text-subtitle">Je suis développeuse Node.js, React et Vue.js. Pour voir mon portfolio, suivez mon regard.</p>
+    </div>
+    <div className="header-picture">
+      <img className="header-picture-content" src={picture} />
     </div>
   </div>
 );

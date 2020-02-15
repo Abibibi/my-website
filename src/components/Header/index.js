c1fb2c8d8062e3581687afcaaea5d37927686c65
Abibibi <abeba.ngwe@gmail.com>
2020-02-16 00:01:15 +0100

    more css improvement

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -7,7 +7,7 @@ const Header = () => (
   <div className="header">
     <div className="header-text">
       <p className="header-text-name">Abeba Ngwe</p>
-      <p className="header-text-subtitle">Je suis développeuse Node.js, React et Vue.js. Pour voir mon portfolio, suivez mon regard.</p>
+      <p className="header-text-subtitle">Mon nom est Abeba Ngwe et je suis <strong className="header-text-subtitle-profession">développeuse Node.js</strong><strong className="header-text-subtitle-profession">, React</strong> et <strong className="header-text-subtitle-profession">Vue.js</strong>. Pour voir mon portfolio, suivez mon regard.</p>
     </div>
     <div className="header-picture">
       <img className="header-picture-content" src={picture} alt="portrait où Abeba Ngwe regarde vers le bas en souriant, donnant l'impression d'observer la section suivante du site qui se trouve justement plus bas." />

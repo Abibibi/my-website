86e738263ef927e8fa1dea7aafa7e55da32ae94c
Abibibi <abeba.ngwe@gmail.com>
2020-02-16 21:50:43 +0100

    menu links size ok

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -7,13 +7,52 @@ import Contact from 'src/styles/assets/images/contact_navbar.png';
 
 const Navbar = () => (
   <div className="navbar">
-    <a className="navbar-website-name" href="#">A. N.</a>
-    <a className="navbar-website-responsivename" href="#">A. N.</a>
+    <a className="navbar-website-name" href="#">A.</a>
+    <a className="navbar-website-responsivename" href="#">A.</a>
     <nav className="navbar-nav">
       <ul className="navbar-nav-links">
-        <li className="navbar-nav-links-link"><a className="navbar-nav-links-link-onelink" href="#"><img className="navbar-nav-links-link-onelink-github" src={GitHub} alt="Logo Github" /></a></li>
-        <li className="navbar-nav-links-link"><a className="navbar-nav-links-link-onelink" href="#"><img className="navbar-nav-links-link-onelink-linkedin" src={LinkedIn} alt="Logo LinkedIn" /></a></li>
-        <li className="navbar-nav-links-link"><a className="navbar-nav-links-link-onelink" href="http://google.com"><img className="navbar-nav-links-link-onelink-contact" src={Contact} alt="Contact" /></a></li>
+        <li className="navbar-nav-links-link">
+          <a
+            className="navbar-nav-links-link-onelink"
+            href="https://github.com/Abibibi"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <img
+              className="navbar-nav-links-link-onelink-github"
+              src={GitHub}
+              alt="Logo Github"
+            />
+          </a>
+        </li>
+        <li className="navbar-nav-links-link">
+          <a
+            className="navbar-nav-links-link-onelink"
+            href="https://www.linkedin.com/in/abebangwe/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <img
+              className="navbar-nav-links-link-onelink-linkedin"
+              src={LinkedIn}
+              alt="Logo LinkedIn"
+            />
+          </a>
+        </li>
+        <li className="navbar-nav-links-link">
+          <a
+            className="navbar-nav-links-link-onelink"
+            href="mailto:abeba.ngwe@gmail.com?Subject=Prise de contact"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <img
+              className="navbar-nav-links-link-onelink-contact"
+              src={Contact}
+              alt="Contact"
+            />
+          </a>
+        </li>
       </ul>
     </nav>
   </div>

0d3163faa9d27f45f328202e21fc07e0ac32fb5e
Abibibi <abeba.ngwe@gmail.com>
2020-02-16 19:19:35 +0100

    worked on navbar links

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -3,6 +3,7 @@ import React from 'react';
 import './navbar.scss';
 import GitHub from 'src/styles/assets/images/github_navbar.png';
 import LinkedIn from 'src/styles/assets/images/linkedin_navbar.png';
+import Contact from 'src/styles/assets/images/contact_navbar.png';
 
 const Navbar = () => (
   <div className="navbar">
@@ -12,7 +13,7 @@ const Navbar = () => (
       <ul className="navbar-nav-links">
         <li className="navbar-nav-links-link"><a className="navbar-nav-links-link-onelink" href="#"><img className="navbar-nav-links-link-onelink-github" src={GitHub} alt="Logo Github" /></a></li>
         <li className="navbar-nav-links-link"><a className="navbar-nav-links-link-onelink" href="#"><img className="navbar-nav-links-link-onelink-linkedin" src={LinkedIn} alt="Logo LinkedIn" /></a></li>
-        <li className="navbar-nav-links-link navbar-nav-links-link-onelink navbar-nav-links-link-onelink-contact"><a href="http://google.com">Contact</a></li>
+        <li className="navbar-nav-links-link"><a className="navbar-nav-links-link-onelink" href="http://google.com"><img className="navbar-nav-links-link-onelink-contact" src={Contact} alt="Contact" /></a></li>
       </ul>
     </nav>
   </div>

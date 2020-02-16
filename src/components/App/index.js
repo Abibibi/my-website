45ab2315dee539442f5047e01f53bf5704f3d8ab
Abibibi <abeba.ngwe@gmail.com>
2020-02-16 18:30:56 +0100

    with menu

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -3,12 +3,14 @@ import React from 'react';
 
 // == Import : local
 import './app.scss';
+import Navbar from 'src/components/Navbar';
 import Header from 'src/components/Header';
 import Projects from 'src/containers/Projects';
 
 // == Composant
 const App = () => (
   <div id="app">
+    <Navbar />
     <Header />
     <Projects />
   </div>
---
+++ src/components/Navbar/index.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+import './navbar.scss';
+import GitHub from 'src/styles/assets/images/github_navbar.png';
+import LinkedIn from 'src/styles/assets/images/linkedin_navbar.png';
+
+const Navbar = () => (
+  <div className="navbar">
+    <a className="navbar-website-name" href="#">A. N.</a>
+    <a className="navbar-website-responsivename" href="#">A. N.</a>
+    <nav className="navbar-nav">
+      <ul className="navbar-nav-links">
+        <li className="navbar-nav-links-link"><a className="navbar-nav-links-link-onelink" href="#"><img className="navbar-nav-links-link-onelink-github" src={GitHub} alt="Logo Github" /></a></li>
+        <li className="navbar-nav-links-link"><a className="navbar-nav-links-link-onelink" href="#"><img className="navbar-nav-links-link-onelink-linkedin" src={LinkedIn} alt="Logo LinkedIn" /></a></li>
+        <li className="navbar-nav-links-link navbar-nav-links-link-onelink navbar-nav-links-link-onelink-contact"><a href="http://google.com">Contact</a></li>
+      </ul>
+    </nav>
+  </div>
+);
+
+export default Navbar;

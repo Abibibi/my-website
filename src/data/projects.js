b1fb4db3d01af6e2c006e7b1905d6a25a2c14cc5
Abibibi <abeba.ngwe@gmail.com>
2020-02-25 19:36:21 +0100

    importing images in projects data file

--- src/data/projects.js
+++ src/data/projects.js
@@ -1,3 +1,37 @@
+import gratitudePicture1 from 'src/styles/assets/images/screenshots/gratitude/device/gratitude_1.png';
+import gratitudePicture2 from 'src/styles/assets/images/screenshots/gratitude/device/gratitude_2.png';
+import gratitudePicture3 from 'src/styles/assets/images/screenshots/gratitude/device/gratitude_3.png';
+import gratitudePicture4 from 'src/styles/assets/images/screenshots/gratitude/device/gratitude_4.png';
+import gratitudePicture5 from 'src/styles/assets/images/screenshots/gratitude/device/gratitude_5.png';
+
+import serialKillerPicture1 from 'src/styles/assets/images/screenshots/serialkiller/device/serialkiller_1.png';
+import serialKillerPicture2 from 'src/styles/assets/images/screenshots/serialkiller/device/serialkiller_2.png';
+import serialKillerPicture3 from 'src/styles/assets/images/screenshots/serialkiller/device/serialkiller_3.png';
+import serialKillerPicture4 from 'src/styles/assets/images/screenshots/serialkiller/device/serialkiller_4.png';
+import serialKillerPicture5 from 'src/styles/assets/images/screenshots/serialkiller/device/serialkiller_5.png';
+import serialKillerPicture6 from 'src/styles/assets/images/screenshots/serialkiller/device/serialkiller_6.png';
+import serialKillerPicture7 from 'src/styles/assets/images/screenshots/serialkiller/device/serialkiller_7.png';
+import serialKillerPicture8 from 'src/styles/assets/images/screenshots/serialkiller/device/serialkiller_8.png';
+import serialKillerPicture9 from 'src/styles/assets/images/screenshots/serialkiller/device/serialkiller_9.png';
+import serialKillerPicture10 from 'src/styles/assets/images/screenshots/serialkiller/device/serialkiller_10.png';
+import serialKillerPicture11 from 'src/styles/assets/images/screenshots/serialkiller/device/serialkiller_11.png';
+
+import mesProchainesVacancesPicture1 from 'src/styles/assets/images/screenshots/mesprochainesvacances/device/mesprochainesvacances_1.png';
+import mesProchainesVacancesPicture2 from 'src/styles/assets/images/screenshots/mesprochainesvacances/device/mesprochainesvacances_2.png';
+import mesProchainesVacancesPicture3 from 'src/styles/assets/images/screenshots/mesprochainesvacances/device/mesprochainesvacances_3.png';
+
+import monAnnee2020Picture1 from 'src/styles/assets/images/screenshots/monannee2020/device/monannee2020_1.png';
+import monAnnee2020Picture2 from 'src/styles/assets/images/screenshots/monannee2020/device/monannee2020_2.png';
+import monAnnee2020Picture3 from 'src/styles/assets/images/screenshots/monannee2020/device/monannee2020_3.png';
+
+import moteurProduit1 from 'src/styles/assets/images/screenshots/moteurproduit/device/moteurproduit_1.png';
+import moteurProduit2 from 'src/styles/assets/images/screenshots/moteurproduit/device/moteurproduit_2.png';
+import moteurProduit3 from 'src/styles/assets/images/screenshots/moteurproduit/device/moteurproduit_3.png';
+
+import assistantImpression1 from 'src/styles/assets/images/screenshots/assistantimpression/device/assistantimpression_1.png';
+import assistantImpression2 from 'src/styles/assets/images/screenshots/assistantimpression/device/assistantimpression_2.png';
+import assistantImpression3 from 'src/styles/assets/images/screenshots/assistantimpression/device/assistantimpression_3.png';
+
 export default [
   {
     id: 1,
@@ -13,27 +47,27 @@ export default [
     pictures: [
       {
         id: 1,
-        bigPictureContent: 'src/styles/assets/images/screenshots/gratitude/device/gratitude_1.png',
+        bigPictureContent: gratitudePicture1,
         bigPictureAlt: 'Page d\'accueil du site Gratitude',
       },
       {
         id: 2,
-        bigPictureContent: 'src/styles/assets/images/screenshots/gratitude/device/gratitude_2.png',
+        bigPictureContent: gratitudePicture2,
         bigPictureAlt: 'Page de création d\'espace du site Gratitude',
       },
       {
         id: 3,
-        bigPictureContent: 'src/styles/assets/images/screenshots/gratitude/device/gratitude_3.png',
+        bigPictureContent: gratitudePicture3,
         bigPictureAlt: 'Page pour rejoindre un espace sur le site Gratitude',
       },
       {
         id: 4,
-        bigPictureContent: 'src/styles/assets/images/screenshots/gratitude/device/gratitude_4.png',
+        bigPictureContent: gratitudePicture4,
         bigPictureAlt: 'Page pour envoyer un message sur le site Gratitude',
       },
       {
         id: 5,
-        bigPictureContent: 'src/styles/assets/images/screenshots/gratitude/device/gratitude_5.png',
+        bigPictureContent: gratitudePicture5,
         bigPictureAlt: 'Page où consulter ses messages reçus sur le site Gratitude',
       },
     ],
@@ -54,57 +88,57 @@ export default [
     pictures: [
       {
         id: 1,
-        smallPictureContent: 'src/styles/assets/images/screenshots/serialkiller/device/serialkiller_1.png',
+        smallPictureContent: serialKillerPicture1,
         smallPictureAlt: 'Page où sélectionner un genre de série',
       },
       {
         id: 2,
-        smallPictureContent: 'src/styles/assets/images/screenshots/serialkiller/device/serialkiller_2.png',
+        smallPictureContent: serialKillerPicture2,
         smallPictureAlt: 'Page de présentation de la série Shrill : la vidéo apparaît en haut de la page',
       },
       {
         id: 3,
-        smallPictureContent: 'src/styles/assets/images/screenshots/serialkiller/device/serialkiller_3.png',
+        smallPictureContent: serialKillerPicture3,
         smallPictureAlt: 'Page de présentation de la série The Handmaid\'s Tale : le résumé apparaît en milieu de page',
       },
       {
         id: 4,
-        smallPictureContent: 'src/styles/assets/images/screenshots/serialkiller/device/serialkiller_4.png',
+        smallPictureContent: serialKillerPicture4,
         smallPictureAlt: 'Page affichant les résultats d\'une recherche de série. La recherche effectuée est "se" et trois résultats apparaissent, dont la série "See" en premier',
       },
       {
         id: 5,
-        smallPictureContent: 'src/styles/assets/images/screenshots/serialkiller/device/serialkiller_5.png',
+        smallPictureContent: serialKillerPicture5,
         smallPictureAlt: 'Fil d\'actualité : il est possible d\'y publier un avis sur une série.',
       },
       {
         id: 6,
-        smallPictureContent: 'src/styles/assets/images/screenshots/serialkiller/device/serialkiller_6.png',
+        smallPictureContent: serialKillerPicture6,
         smallPictureAlt: 'Fil d\'actualité : il est possible d\'y consulter tous les avis émis par les membres du site',
       },
       {
         id: 7,
-        smallPictureContent: 'src/styles/assets/images/screenshots/serialkiller/device/serialkiller_7.png',
+        smallPictureContent: serialKillerPicture7,
         smallPictureAlt: 'Page où apparaissent les résultats d\'une recherche de membre. La recherche effectuée est "j" et le profil qui est affiché est celui du membre Juju, dont il est possible de se désabonner.',
       },
       {
         id: 8,
-        smallPictureContent: 'src/styles/assets/images/screenshots/serialkiller/device/serialkiller_8.png',
+        smallPictureContent: serialKillerPicture8,
         smallPictureAlt: 'Page de profil du membre connecté en sessions. Y apparaissent une bannière, une photo de profil, le pseudo et les "infos" de l\'utilisateur (Prénom, nom, adresse e-mail)',
       },
       {
         id: 9,
-        smallPictureContent: 'src/styles/assets/images/screenshots/serialkiller/device/serialkiller_9.png',
+        smallPictureContent: serialKillerPicture9,
         smallPictureAlt: 'Jeu disponible sur le profil d\'un utilisateur : au clic sur un bouton, le taux d\'addiction aux séries est révélé',
       },
       {
         id: 10,
-        smallPictureContent: 'src/styles/assets/images/screenshots/serialkiller/device/serialkiller_10.png',
+        smallPictureContent: serialKillerPicture10,
         smallPictureAlt: 'Abonnements de l\'utilisateur connecté en session.',
       },
       {
         id: 11,
-        smallPictureContent: 'src/styles/assets/images/screenshots/serialkiller/device/serialkiller_11.png',
+        smallPictureContent: serialKillerPicture11,
         smallPictureAlt: 'Messagerie instantanée, disponible sur toutes les pages',
       },
     ],
@@ -123,17 +157,17 @@ export default [
     pictures: [
       {
         id: 1,
-        bigPictureContent: 'src/styles/assets/images/screenshots/mesprochainesvacances/device/mesprochainesvacances_1.png',
+        bigPictureContent: mesProchainesVacancesPicture1,
         bigPictureAlt: 'Formulaire où renseigner son prénom (ici, Abi) ainsi que son prochain lieu (ici, Kinshasa) et sa prochaine date de séjour (ici, le 8 mars 2020)',
       },
       {
         id: 2,
-        bigPictureContent: 'src/styles/assets/images/screenshots/mesprochainesvacances/device/mesprochainesvacances_2.png',
+        bigPictureContent: mesProchainesVacancesPicture2,
         bigPictureAlt: 'Affichage d\'une photo en arrière-plan représentant le lieu de séjour et d\'un compte à rebours d\'ici la date de séjour (soit d\'ici le 8 mars 2020)',
       },
       {
         id: 3,
-        bigPictureContent: 'src/styles/assets/images/screenshots/mesprochainesvacances/device/mesprochainesvacances_3.png',
+        bigPictureContent: mesProchainesVacancesPicture3,
         bigPictureAlt: 'Affichage d\'une photo en arrière-plan représentant le lieu de séjour et d\'un compte à rebours d\'ici une date déjà passée. Le compte à rebours affiche 00:00:00',
       },
     ],
@@ -151,17 +185,17 @@ export default [
     pictures: [
       {
         id: 1,
-        smallPictureContent: 'src/styles/assets/images/screenshots/monannee2020/device/monannee2020_1.png',
+        smallPictureContent: monAnnee2020Picture1,
         smallPictureAlt: 'Affichage d\'une prédiction pour 2020 : "En 2020, je... stalkerai mes collègues sur GitHub"',
       },
       {
         id: 2,
-        smallPictureContent: 'src/styles/assets/images/screenshots/monannee2020/device/monannee2020_2.png',
+        smallPictureContent: monAnnee2020Picture2,
         smallPictureAlt: 'Affichage d\'une prédiction pour 2020 : "En 2020, je... partagerai mon code"',
       },
       {
         id: 3,
-        smallPictureContent: 'src/styles/assets/images/screenshots/monannee2020/device/monannee2020_3.png',
+        smallPictureContent: monAnnee2020Picture3,
         smallPictureAlt: 'Affichage d\'une prédiction pour 2020 : "En 2020, je... continuerai le cardio-kickboxing"',
       },
     ],
@@ -178,17 +212,17 @@ export default [
     pictures: [
       {
         id: 1,
-        bigPictureContent: 'src/styles/assets/images/screenshots/moteurproduit/device/moteurproduit_1.png',
+        bigPictureContent: moteurProduit1,
         bigPictureAlt: 'Page d\'accueil où apparaissent des suggestions de produits capillaires',
       },
       {
         id: 2,
-        bigPictureContent: 'src/styles/assets/images/screenshots/moteurproduit/device/moteurproduit_2.png',
+        bigPictureContent: moteurProduit2,
         bigPictureAlt: 'Page où apparaissent tous les laits capillaires',
       },
       {
         id: 3,
-        bigPictureContent: 'src/styles/assets/images/screenshots/moteurproduit/device/moteurproduit_3.png',
+        bigPictureContent: moteurProduit3,
         bigPictureAlt: 'Page où apparaissent tous les après-shampoings en-dessous de 15€ après soumission d\'un formulaire par l\'utilisateur ou l\'utilisatrice',
       },
     ],
@@ -205,17 +239,17 @@ export default [
     pictures: [
       {
         id: 1,
-        smallPictureContent: 'src/styles/assets/images/screenshots/assistantimpression/device/assistantimpression_1.png',
+        smallPictureContent: assistantImpression1,
         smallPictureAlt: 'Formulaire où renseigner un chiffre minimal et un chiffre maximal pour obtenir une suite de chiffres soit paire soit impaire, ce qui est pratique pour les impressions recto-verso.',
       },
       {
         id: 2,
-        smallPictureContent: 'src/styles/assets/images/screenshots/assistantimpression/device/assistantimpression_2.png',
+        smallPictureContent: assistantImpression2,
         smallPictureAlt: 'Affichage d\'une suite impaire après soumission du formulaire',
       },
       {
         id: 3,
-        smallPictureContent: 'src/styles/assets/images/screenshots/assistantimpression/device/assistantimpression_3.png',
+        smallPictureContent: assistantImpression3,
         smallPictureAlt: 'Affichage d\'une suite paire après soumission du formulaire',
       },
     ],

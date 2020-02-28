2ecd46e94fe89b691b3089a98283ef4aac3a027c
Abibibi <abeba.ngwe@gmail.com>
2020-02-28 23:46:58 +0100

    corrected height and width values in meta elemtn

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -16,7 +16,7 @@ const App = () => {
       const viewheight = window.innerHeight;
       const viewwidth = window.innerWidth;
       const viewport = document.querySelector('meta[name=viewport]');
-      viewport.setAttribute('content', `height="${viewheight}px, width=${viewwidth}px, initial-scale=1.0"`);
+      viewport.setAttribute('content', `height=${viewheight}px, width=${viewwidth}px, initial-scale=1.0`);
     }, 300);
   }, []);
 

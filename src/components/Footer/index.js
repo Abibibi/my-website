54f95c41a59332caf3b7405bf10a0d18d1482ea0
Abibibi <abeba.ngwe@gmail.com>
2020-03-14 01:03:29 +0100

    a new cookie banner

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -25,15 +25,6 @@ const Footer = () => (
             LinkedIn
           </a>
         </p>
-        <p>
-          <a
-            href="javascript:showAxeptioButton()"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Cliquez-ici pour modifier vos préférences en matière de cookies
-          </a>
-        </p>
       </div>
       <div className="footer-container-copyright">
         <p>Abeba Ngwe - 2020 &#169;</p>
